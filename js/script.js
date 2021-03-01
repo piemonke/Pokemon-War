9273a2b9ccaf717d177b4ec11d89353d32f6b7c5
Dylan Sharon <reachdylan@gmail.com>
2021-03-01 10:30:31 -0500

    created initial html tags and js functions for game of war

--- js/script.js
+++ js/script.js
@@ -1,3 +1,27 @@
 // alert("JS is loaded");
 // console.log($);
 
+//variables
+
+
+//functions
+
+//deals card from deck
+function deal() {
+
+}
+
+//shuffles each players deck when creating
+function shuffle(cards) {
+
+}
+
+//targets player and computers dealt cards, returns which card wins, updates tiles accordingly
+function compare() {
+
+}
+
+//adds cards won to winnings deck
+function addToWinnings() {
+
+}
