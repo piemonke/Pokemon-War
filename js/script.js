d78ea0f51971462af15bc5a112f04c4a47c4cf89
Dylan Sharon <reachdylan@gmail.com>
2021-03-05 15:10:51 -0500

    Play function written, assigned to event listener on button with id=play

--- js/script.js
+++ js/script.js
@@ -13,6 +13,8 @@ let playerHand = [];
 let playerWins = [];
 let compHand = [];
 let compWins = [];
+let cardPlayer;
+let cardComp;
 
 
 
@@ -24,77 +26,7 @@ getPokeList();
 //function to get number of pokemon in API
 //nested pull request puts all data into array
 function getPokeList() {
-    $.ajax({
-        url:"https://pokeapi.co/api/v2/pokemon"
-    }).then(
-        (data) => {
-            // console.log(data.count);
-            length = data.count;
-            $.ajax({
-                url:"https://pokeapi.co/api/v2/pokemon?offset=0&limit=" + length
-            }).then(
-                (data) => {
-                    // console.log(data);
-                    pokeList = (data.results);
-                    //currently array holds only pokemon name and url
-                    //require pokemon stats
-                    //need to iterate over each array index to pull stats from API
-                    //also want to remove gimmick pokemon (gmax, mega evolutions, etc)--accomplished
-                    pokeList.forEach(function(pokemon, index) {
-                        if(pokemon.name.includes("-gmax") || pokemon.name.includes("-mega")) {
-                            removedPoke.push(pokeList.splice(index, 1));
-                        }
-                        //pull stats and typing from API
-                        //add to each object in array
-                        //NOTE: At the time of writing, 3 urls in this API are broken and returning 404 errors. Since their data cannot be pulled, I will be removing them from the pokeList array so that they do not cause errors in the actual game. This code has been added to the error catch so that any future broken urls are also caught.
-                        $.ajax({
-                            url:pokemon.url
-                        }).then(
-                            (data) => {
-                                // console.log(data);
-                                //store url for sprite data
-                                pokemon.sprite = data.sprites.front_default;
-
-                                //store all base stats added together
-                                let statTotal = 0;
-                                data.stats.forEach(stat => {
-                                    statTotal += stat.base_stat;
-                                });
-                                pokemon.stats = statTotal;
-
-                                //store type data
-                                // pokemon.types = data.types;
-                                let types = []
-                                data.types.forEach(type => {
-                                    types.push(type.type.name);
-                                });
-                                pokemon.types = types;
-
-                                //delete url, no longer needed
-                                delete pokemon.url;
-                            },
-                            (error) => {
-                                console.log("Pokemon data pull request error", error);
-                                removedPoke.push(pokeList.splice(index, 1));
-                            }
-                        )
-                    });
-                    // console.log(pokeList);
-                    
-                    
-                    //function calls for the game go here now
-                    deal(pokeList, playerHand, compHand);
-                    play(playerHand, compHand);
-                },
-                (error) => {
-                    console.log("Pokemon List Request Denied", error);
-                }
-            )
-        },
-        (error) => {
-            console.log("Bad Pull Request", error);
-        }
-    );
+    
 }
 
 //type data that will setup later, this web will be annoying, need lots of .contains chec
@@ -108,6 +40,90 @@ function getPokeList() {
 //         console.log("type data pull error")
 //     }
 // )
+$.ajax({
+    url:"https://pokeapi.co/api/v2/pokemon"
+}).then(
+    (data) => {
+        // console.log(data.count);
+        length = data.count;
+        $.ajax({
+            url:"https://pokeapi.co/api/v2/pokemon?offset=0&limit=" + length
+        }).then(
+            (data) => {
+                pokeList = (data.results);
+                //currently array holds only pokemon name and url
+                //require pokemon stats
+                //need to iterate over each array index to pull stats from API           
+                
+                
+                pokeList.forEach(function(pokemon, index) {
+                    //pull stats and typing from API
+                    //add to each object in array
+                    //NOTE: At the time of writing, 3 urls in this API are broken and returning 404 errors. Since their data cannot be pulled, I will be removing them from the pokeList array so that they do not cause errors in the actual game. This code has been added to the error catch so that any future broken urls are also caught.
+                    $.ajax({
+                        url:pokemon.url
+                    }).then(
+                        (data) => {
+                            // console.log(data);
+                            //store url for sprite data
+                            pokemon.sprite = data.sprites.front_default;
+
+                            //store all base stats added together
+                            let statTotal = 0;
+                            data.stats.forEach(stat => {
+                                statTotal += stat.base_stat;
+                            });
+                            pokemon.stats = statTotal;
+
+                            //store type data
+                            // pokemon.types = data.types;
+                            let types = []
+                            data.types.forEach(type => {
+                                types.push(type.type.name);
+                            });
+                            pokemon.types = types;
+
+                            //delete url, no longer needed
+                            delete pokemon.url;
+                        },
+                        (error) => {
+                            console.log("Pokemon data pull request error", error);
+                            // removedPoke.push(pokeList.splice(index, 1)[0]);
+                        }
+                        
+                    )
+                });//end of forEach loop
+                
+            },
+            (error) => {
+                console.log("Pokemon List Request Denied", error);
+            }
+        ).then((response) => {
+            // pokeList = pokeList.filter(pokemon => pokemon.url);
+            console.log(pokeList);
+            deal(pokeList, playerHand, compHand);
+            $("#play").on("click", function() {
+                play()
+            });
+        });
+        
+    },
+    (error) => {
+        console.log("Bad Pull Request", error);
+    }
+);
+
+
+
+
+
+
+
+
+
+
+
+
 
 //main game function for function calls
 function main() {
@@ -116,7 +132,8 @@ function main() {
 
 //deals card from main deck into each players hand
 function deal(deck, player, computer) {
-    while(deck.length) {
+    // console.log(deck);
+    while(deck.length > 0) {
         player.push(deck.splice(Math.floor(Math.random() * deck.length), 1)[0]);
         computer.push(deck.splice(Math.floor(Math.random() * deck.length), 1)[0]);
     }
@@ -124,12 +141,18 @@ function deal(deck, player, computer) {
 }
 
 //unshifts card from each deck, puts card on the field
-function play(player, computer) {
-    var cardPlayer = player.shift();
-    var cardComp = computer.shift();
-    
-    $("#playercard").html(`<h1>${cardPlayer.name}</h1>`)
-    $("#compcard").html(`<h1>${cardComp.name}</h1>`)
+function play() {
+    // console.log(playerHand);
+    // console.log(compHand);
+    cardPlayer = playerHand.shift();
+    cardComp = compHand.shift();
+    // console.log(cardPlayer.stats);
+    // console.log(cardComp.stats);
+    $("#playercard").html(`<h1>${cardPlayer.name}</h1><br><p>${cardPlayer.stats}</p>`)
+    $("#compcard").html(`<h1>${cardComp.name}</h1><br><p>${cardComp.stats}</p>`)
+
+    //call compare function to keep variables in scope
+    compare(cardPlayer, cardComp);
 }
 
 //shuffles each players deck when creating
@@ -138,12 +161,20 @@ function shuffle(discarded, hand) {
 }
 
 //targets player and computers dealt cards, returns which card wins, updates tiles accordingly
-function compare() {
-
+function compare(player, comp) {
+    console.log(player);
+    console.log(comp);
+    if(player.stats === comp.stats) {
+        console.log("tie");
+    } else if(player.stats > comp.stats) {
+        console.log("player wins");
+    } else {
+        console.log("computer wins");
+    }
 }
 
 //adds cards won to winnings deck
-function addToWinnings() {
-
+function addToWinnings(winnerDeck, cardOne, cardTwo) {
+    winnerDeck.push(cardOne, cardTwo);
 }
 
