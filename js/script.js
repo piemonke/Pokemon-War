fedf2016b47a0f477f17c2eccdee59ffa4448eb7
Dylan Sharon <reachdylan@gmail.com>
2021-03-05 16:05:34 -0500

    set limit of pulls to 30 to test game logic, game capable of declaring winner, shuffling decks. Features still to be added: new game button, full implementation of war function, adding type advantage modifiers to stats

--- js/script.js
+++ js/script.js
@@ -44,8 +44,10 @@ $.ajax({
     url:"https://pokeapi.co/api/v2/pokemon"
 }).then(
     (data) => {
-        // console.log(data.count);
-        length = data.count;
+        //uncomment below for delivery
+        //let length = data.count;
+        //setting length to static ammount to make testing not take ages
+        let length = 30;
         $.ajax({
             url:"https://pokeapi.co/api/v2/pokemon?offset=0&limit=" + length
         }).then(
@@ -56,7 +58,7 @@ $.ajax({
                 //need to iterate over each array index to pull stats from API           
                 
                 
-                pokeList.forEach(function(pokemon, index) {
+                pokeList.forEach(function(pokemon) {
                     //pull stats and typing from API
                     //add to each object in array
                     //NOTE: At the time of writing, 3 urls in this API are broken and returning 404 errors. Since their data cannot be pulled, I will be removing them from the pokeList array so that they do not cause errors in the actual game. This code has been added to the error catch so that any future broken urls are also caught.
@@ -88,7 +90,6 @@ $.ajax({
                         },
                         (error) => {
                             console.log("Pokemon data pull request error", error);
-                            // removedPoke.push(pokeList.splice(index, 1)[0]);
                         }
                         
                     )
@@ -99,12 +100,9 @@ $.ajax({
                 console.log("Pokemon List Request Denied", error);
             }
         ).then((response) => {
-            // pokeList = pokeList.filter(pokemon => pokemon.url);
-            console.log(pokeList);
-            deal(pokeList, playerHand, compHand);
-            $("#play").on("click", function() {
-                play()
-            });
+            $("#start").on("click", function() {
+                main();
+            })
         });
         
     },
@@ -127,7 +125,11 @@ $.ajax({
 
 //main game function for function calls
 function main() {
-
+    pokeList = pokeList.filter(pokemon => !pokemon.hasOwnProperty("url"));
+    deal(pokeList, playerHand, compHand);
+    $("#play").on("click", function() {
+        play()
+    });
 }
 
 //deals card from main deck into each players hand
@@ -141,40 +143,71 @@ function deal(deck, player, computer) {
 }
 
 //unshifts card from each deck, puts card on the field
+//handles DOM manipulation, calls comparison function
 function play() {
-    // console.log(playerHand);
-    // console.log(compHand);
-    cardPlayer = playerHand.shift();
-    cardComp = compHand.shift();
-    // console.log(cardPlayer.stats);
-    // console.log(cardComp.stats);
-    $("#playercard").html(`<h1>${cardPlayer.name}</h1><br><p>${cardPlayer.stats}</p>`)
-    $("#compcard").html(`<h1>${cardComp.name}</h1><br><p>${cardComp.stats}</p>`)
+    //if either players hands AND winnings are both empty, declare victory
+    //else continue with function 
+    if(playerHand.length === 0 && playerWins.length === 0) {
+        console.log("Computer Victory")
+    } else if(compHand.length === 0 && compWins.length === 0) {
+        console.log("Player Victory");
+    } else {
+        //if either hand is empty, shuffle hands
+        //NO ELSE statement as both hands might need to be shuffled at the same time
+        if(playerHand.length === 0) {
+            shuffle(playerWins, playerHand);
+        }
+        if(compHand.length === 0) {
+            shuffle(compWins, compHand);
+        }
 
-    //call compare function to keep variables in scope
-    compare(cardPlayer, cardComp);
+        cardPlayer = playerHand.shift();
+        cardComp = compHand.shift();
+        // console.log(cardPlayer.stats);
+        // console.log(cardComp.stats);
+        $("#playercard").html(`<h1>${cardPlayer.name}</h1><br><p>${cardPlayer.stats}</p>`)
+        $("#compcard").html(`<h1>${cardComp.name}</h1><br><p>${cardComp.stats}</p>`)
+
+        //call compare function to keep variables in scope
+        compare(cardPlayer, cardComp);
+    }
 }
 
 //shuffles each players deck when creating
-function shuffle(discarded, hand) {
-
+function shuffle(oldHand, newHand) {
+    while(oldHand.length) {
+        newHand.push(oldHand.splice(Math.floor(Math.random() * oldHand.length), 1)[0])
+    }
 }
 
-//targets player and computers dealt cards, returns which card wins, updates tiles accordingly
+//takes cards as input, compares, updates winner
 function compare(player, comp) {
-    console.log(player);
-    console.log(comp);
+    let pot = [];
+    pot.push(player, comp)
     if(player.stats === comp.stats) {
         console.log("tie");
+        //call war function
+        war(pot);
     } else if(player.stats > comp.stats) {
         console.log("player wins");
+        //give cards to player deck
+        addToWinnings(playerWins, pot);
     } else {
         console.log("computer wins");
+        //give cards to comp deck
+        addToWinnings(compWins, pot);
     }
 }
 
 //adds cards won to winnings deck
-function addToWinnings(winnerDeck, cardOne, cardTwo) {
-    winnerDeck.push(cardOne, cardTwo);
+function addToWinnings(winnerDeck, cards) {
+    winnerDeck.push(...cards);
 }
 
+function war(bigPot) {
+    for(i = 0; i > 3; i++) {
+        bigPot.push(playerHand.shift());
+        bigPot.push(compHand.shift());
+    }
+    play();
+}
